5268ecff87a6ce6acbf257c21c81dbd5ff9f6859
Jhayr Cervantes <jhayrcervantes0981@gmail.com>
2026-09-25 13:26:05 +0800

    feat: fix

---
+++ app/Http/Requests/UpdateWorkScheduleRequest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Requests;
+
+class UpdateWorkScheduleRequest extends ApiFormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'days' => ['required', 'array', 'min:1', 'max:7'],
+            'days.*.day_of_week' => ['required', 'integer', 'min:0', 'max:6', 'distinct'],
+            'days.*.is_working_day' => ['required', 'boolean'],
+            'days.*.start_time' => ['nullable', 'required_if:days.*.is_working_day,true', 'date_format:H:i'],
+            'days.*.end_time' => ['nullable', 'required_if:days.*.is_working_day,true', 'date_format:H:i'],
+            'days.*.break_minutes' => ['nullable', 'integer', 'min:0', 'max:480'],
+        ];
+    }
+}
