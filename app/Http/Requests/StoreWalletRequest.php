745733782315034af2a467545fd4e4bac41a389f
Jhayr Cervantes <jhayrcervantes0981@gmail.com>
2026-09-25 22:07:07 +0800

    feat: fix

--- app/Http/Requests/StoreWalletRequest.php
+++ app/Http/Requests/StoreWalletRequest.php
@@ -19,6 +19,12 @@ class StoreWalletRequest extends ApiFormRequest
         return [
             'name' => [$required, 'string', 'max:60'],
             'type' => ['nullable', Rule::in(Wallet::TYPES)],
+            'category' => ['nullable', Rule::in(Wallet::CATEGORIES)],
+            'institution_id' => ['nullable', 'string', 'max:40', 'regex:/^[a-z0-9_-]+$/'],
+            'account_type' => ['nullable', Rule::in(Wallet::ACCOUNT_TYPES)],
+            'last4' => ['nullable', 'string', 'regex:/^[0-9]{4}$/'],
+            'holder_name' => ['nullable', 'string', 'max:100'],
+            'color' => ['nullable', 'string', 'regex:/^#[0-9A-Fa-f]{6}$/'],
             'opening_balance' => ['nullable', 'numeric', 'min:0', 'max:999999999'],
             'balance_as_of' => ['nullable', 'date_format:Y-m-d'],
             'receives_salary' => ['nullable', 'boolean'],
