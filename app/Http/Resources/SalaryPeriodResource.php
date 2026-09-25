bc9e254b96b7ca90a457776bb347b838a2f788d0
Jhayr Cervantes <jhayrcervantes0981@gmail.com>
2026-09-25 13:26:38 +0800

    feat: fix

---
+++ app/Http/Resources/SalaryPeriodResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\SalaryPeriod;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/** @mixin SalaryPeriod */
+class SalaryPeriodResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'period_type' => $this->period_type,
+            'start_date' => $this->start_date?->toDateString(),
+            'end_date' => $this->end_date?->toDateString(),
+            'status' => $this->status,
+            'summary' => $this->when(isset($this->summary), fn () => $this->summary),
+        ];
+    }
+}
