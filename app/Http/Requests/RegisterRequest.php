8ab61ce5aa93a7f0b7eb54a8f25eca01f1041a58
Jhayr Cervantes <jhayrcervantes0981@gmail.com>
2026-09-25 13:25:24 +0800

    feat: fix

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Validation\Rules\Password;
+
+class RegisterRequest extends ApiFormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:100'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email'],
+            'password' => ['required', 'string', Password::min(8)],
+            'device_name' => ['nullable', 'string', 'max:100'],
+        ];
+    }
+}
