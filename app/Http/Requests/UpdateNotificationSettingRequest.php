2867aaccecd6d92a07b38a1b78b5d8f89cb5082b
Jhayr Cervantes <jhayrcervantes0981@gmail.com>
2026-09-25 13:25:54 +0800

    feat: fix

---
+++ app/Http/Requests/UpdateNotificationSettingRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests;
+
+class UpdateNotificationSettingRequest extends ApiFormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'work_notification' => ['nullable', 'boolean'],
+            'work_notification_lead_minutes' => ['nullable', 'integer', 'min:0', 'max:720'],
+            'before_work_reminder' => ['nullable', 'boolean'],
+            'before_work_minutes' => ['nullable', 'integer', 'min:1', 'max:240'],
+            'still_on_duty_reminder' => ['nullable', 'boolean'],
+            'still_on_duty_minutes' => ['nullable', 'integer', 'min:1', 'max:600'],
+            'salary_period_reminder' => ['nullable', 'boolean'],
+            'expense_reminder' => ['nullable', 'boolean'],
+            'expense_reminder_time' => ['nullable', 'date_format:H:i'],
+        ];
+    }
+}
