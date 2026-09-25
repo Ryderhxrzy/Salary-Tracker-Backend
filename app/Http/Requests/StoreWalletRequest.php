f8603be66a0e931b49d243de2a5ab6226e3c224f
Jhayr Cervantes <jhayrcervantes0981@gmail.com>
2026-09-25 23:57:58 +0800

    feat: fix

--- app/Http/Requests/StoreWalletRequest.php
+++ app/Http/Requests/StoreWalletRequest.php
@@ -25,6 +25,13 @@ class StoreWalletRequest extends ApiFormRequest
             'last4' => ['nullable', 'string', 'regex:/^[0-9]{4}$/'],
             'holder_name' => ['nullable', 'string', 'max:100'],
             'color' => ['nullable', 'string', 'regex:/^#[0-9A-Fa-f]{6}$/'],
+            // Card look chosen by the user; null resets to the institution look.
+            'design' => ['nullable', 'array'],
+            'design.mode' => ['required_with:design', Rule::in(Wallet::DESIGN_MODES)],
+            'design.colors' => ['required_with:design', 'array', 'min:1', 'max:3'],
+            'design.colors.*' => ['string', 'regex:/^#[0-9A-Fa-f]{6}$/'],
+            'design.direction' => ['required_with:design', Rule::in(Wallet::DESIGN_DIRECTIONS)],
+            'design.pattern' => ['required_with:design', Rule::in(Wallet::DESIGN_PATTERNS)],
             'opening_balance' => ['nullable', 'numeric', 'min:0', 'max:999999999'],
             'balance_as_of' => ['nullable', 'date_format:Y-m-d'],
             'receives_salary' => ['nullable', 'boolean'],
