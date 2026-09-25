a8a5de4e07fb301619fc8fef5b16da6fd3180716
Jhayr Cervantes <jhayrcervantes0981@gmail.com>
2026-09-25 13:25:58 +0800

    feat: fix

---
+++ app/Http/Requests/UpdateProfileRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\EmployeeProfile;
+use Illuminate\Validation\Rule;
+
+class UpdateProfileRequest extends ApiFormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'full_name' => ['nullable', 'string', 'max:150'],
+            'nickname' => ['nullable', 'string', 'max:60'],
+            'position' => ['nullable', 'string', 'max:150'],
+            'company' => ['nullable', 'string', 'max:150'],
+            'employment_type' => ['nullable', Rule::in(EmployeeProfile::EMPLOYMENT_TYPES)],
+            'timezone' => ['nullable', 'timezone:all'],
+            'currency' => ['nullable', 'string', 'size:3'],
+            'late_grace_minutes' => ['nullable', 'integer', 'min:0', 'max:240'],
+        ];
+    }
+}
