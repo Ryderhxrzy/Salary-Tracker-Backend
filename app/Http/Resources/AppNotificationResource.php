a7979aaf031a8610233026ab76cb0bdbfeaa831b
Jhayr Cervantes <jhayrcervantes0981@gmail.com>
2026-09-26 11:00:47 +0800

    feat: fix

---
+++ app/Http/Resources/AppNotificationResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\AppNotification;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/** @mixin AppNotification */
+class AppNotificationResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'type' => $this->type,
+            'title' => $this->title,
+            'body' => $this->body,
+            'data' => $this->data,
+            'is_read' => $this->read_at !== null,
+            'created_at' => $this->created_at?->toIso8601String(),
+        ];
+    }
+}
